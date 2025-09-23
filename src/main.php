d8d1564c89caea472de0382f9eef6f5f520c3154
Azhega <lethan986@gmail.com>
2025-09-23 15:35:30 +0200

    Init Main Folders

---
+++ src/main.php
@@ -0,0 +1,11 @@
+<?php
+use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Jugid\Staurie\Staurie;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$staurie = new Staurie('My game');
+$staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
+$staurie->run();
